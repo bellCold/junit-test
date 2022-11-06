36a50f16b2ab7e65c1765c2d43a976bf6a077291
whdcks420 <whdcks420@gmail.com>
2022-11-06 19:35:42 +0900

    custom tag

--- src/test/java/com/example/javatest/CustomTagTest.java
+++ src/test/java/com/example/javatest/CustomTagTest.java
@@ -1,17 +1,21 @@
 package com.example.javatest;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
-public class TagTest {
+public class CustomTagTest {
 
-    @FastTest
+    @Test
     @DisplayName("스터디 만들기 fast")
+    @Tag("fast")
     void create() {
         System.out.println("create");
     }
 
-    @SlowTest
+    @Test
     @DisplayName("스터디 만들기 slow")
+    @Tag("slow")
     void create1() {
         System.out.println("create1");
     }
